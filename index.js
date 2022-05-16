7941c1758f7f290b34bacf37fe21b1a2a55b78db
Gerel Budaeva <gerel.budaeva@gmail.com>
2022-05-16 10:06:06 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,45 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+const routes = require("./routes");
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-
-let cards = [
-    {id: '1', name: 'First card', status: 'todo', priority: 1},
-    {id: '2', name: 'second card', status: 'progress', priority: 4},
-    {id: '3', name: 'Next card', status: 'done', priority: 5},
-    {id: '4', name: 'Nice card', status: 'preview', priority: 10},
-]
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.get('/cards', (req, res) => {
-    res.send(cards);
-})
-
-app.delete('/cards/:cardId', (req, res) => {
-    const cardId = req.params.cardId;
-    cards = cards.filter(el => el.id !== cardId);
-    res.send(cards);
-})
-
-app.post('/cards', (req, res) => {
-    const card = req.body;
-    cards.push({ id: Math.random().toString(), ...card});
-    res.send('Card created');
-})
-
-app.patch('/cards/:cardId', (req, res) => {
-    const cardId = req.params.cardId;
-    const card = req.body;
-    cards = cards.map(el => el.id === cardId ? ({...card, id: el.id}) : el);
-    res.send('Card updated');
-})
+routes(app);
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
